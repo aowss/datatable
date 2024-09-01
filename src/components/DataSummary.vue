11d0468dea95aa653e70082ec9b1618425a401e8
Aowss Ibrahim <aowss@yahoo.com>
2024-09-01 17:01:21 -0400

    Using a card overlay for summary and view

--- src/components/DataSummary.vue
+++ src/components/DataSummary.vue
@@ -12,6 +12,13 @@ const props = defineProps({
 
 const AsyncFunction = Object.getPrototypeOf(async function () {}).constructor
 
+const rangeLabel = (min, max, uniqueValues) =>
+  uniqueValues.size === 1
+    ? uniqueValues.values().next().value
+    : uniqueValues.size <= 3
+      ? Array.from(uniqueValues)
+      : `From '${('' + min).length <= 5 ? ('' + min).substring(0, 5) : ('' + min).substring(0, 5).concat('...')}' to '${('' + max).length <= 5 ? ('' + max).substring(0, 5) : ('' + max).substring(0, 5).concat('...')}'`
+
 const summarize = (data) => {
   const result = []
   const dataByKey = data.values.reduce((acc, cur) => {
@@ -36,12 +43,7 @@ const summarize = (data) => {
       range: {
         min: min,
         max: max,
-        label:
-          uniqueValues.size === 1
-            ? uniqueValues.values().next().value
-            : uniqueValues.size <= 3
-              ? Array.from(uniqueValues)
-              : `From '${('' + min).length <= 5 ? ('' + min).substring(0, 5) : ('' + min).substring(0, 5).concat('...')}' to '${('' + max).length <= 5 ? ('' + max).substring(0, 5) : ('' + max).substring(0, 5).concat('...')}'`
+        label: rangeLabel(min, max, uniqueValues)
       },
       uniqueValues: uniqueValues,
       validationCode: '',
@@ -71,58 +73,67 @@ const onCellEditComplete = (event) => {
 </script>
 
 <template>
-  <div>
-    <Tag icon="pi pi-bars" severity="contrast" :value="`Row Count: ${data.rowsCount}`" /> &nbsp;
-    <Tag icon="pi pi-arrows-h" severity="contrast" :value="`Column Count: ${data.columnsCount}`" />
-    &nbsp;
-    <br />
-    <DataTable
-      :value="summary"
-      size="small"
-      stripedRows
-      editMode="cell"
-      @cell-edit-complete="onCellEditComplete"
-    >
-      <Column field="name" header="Name" />
-      <Column field="type" header="Type">
-        <template #body="{ data }">
-          <Tag :value="data.type" v-tooltip="data.format" />
-        </template>
-      </Column>
-      <Column field="notEmpty" header="Has a value?">
-        <template #body="slotProps">
-          <PercentageComponent :total="data.rowsCount" :count="slotProps.data.notEmpty" />
-        </template>
-      </Column>
-      <Column field="range.label" header="Range" />
-      <Column field="unique" header="Unique Values">
-        <template #body="slotProps">
-          <PercentageComponent :total="data.rowsCount" :count="slotProps.data.uniqueValues.size" />
-        </template>
-      </Column>
-      <Column field="validationCode" header="Validation Code">
-        <template #editor="{ data, field }">
-          <Textarea
-            id="code"
-            v-model="data[field]"
-            variant="filled"
-            autoResize
-            rows="2"
-            cols="30"
-            :placeholder="`return value.startsWith('A') // signature: async function(value: ${data.type.replace(',', ' |')}): boolean`"
-            fluid
-          />
-        </template>
-      </Column>
-      <Column header="Valid">
-        <template #body="slotProps">
-          <PercentageComponent
-            v-if="readyToValidate"
-            :total="slotProps.data.uniqueValues.size"
-            :count="slotProps.data.validValues.filter((result) => result === true).length"
-          />
-        </template>
-      </Column>
-    </DataTable>
+  <div data-cy="dataSummary" class="card">
+    <div class="card-img-overlay" style="width: 100%">
+      <Tag icon="pi pi-bars" severity="contrast" :value="`Row Count: ${data.rowsCount}`" /> &nbsp;
+      <Tag
+        icon="pi pi-arrows-h"
+        severity="contrast"
+        :value="`Column Count: ${data.columnsCount}`"
+      />
+      &nbsp;
+      <br />
+      <DataTable
+        :value="summary"
+        size="small"
+        stripedRows
+        editMode="cell"
+        @cell-edit-complete="onCellEditComplete"
+      >
+        <Column field="name" header="Name" />
+        <Column field="type" header="Type">
+          <template #body="{ data }">
+            <Tag :value="data.type" v-tooltip="data.format" />
+          </template>
+        </Column>
+        <Column field="notEmpty" header="Has a value?">
+          <template #body="slotProps">
+            <PercentageComponent :total="data.rowsCount" :count="slotProps.data.notEmpty" />
+          </template>
+        </Column>
+        <Column field="range.label" header="Range" />
+        <Column field="unique" header="Unique Values">
+          <template #body="slotProps">
+            <PercentageComponent
+              :total="data.rowsCount"
+              :count="slotProps.data.uniqueValues.size"
+            />
+          </template>
+        </Column>
+        <Column field="validationCode" header="Validation Code">
+          <template #editor="{ data, field }">
+            <Textarea
+              id="code"
+              v-model="data[field]"
+              variant="filled"
+              autoResize
+              rows="2"
+              cols="30"
+              :placeholder="`return value.startsWith('A') // signature: async function(value: ${data.type.replace(',', ' |')}): boolean`"
+              fluid
+            />
+          </template>
+        </Column>
+        <Column header="Valid">
+          <template #body="slotProps">
+            <PercentageComponent
+              v-if="readyToValidate"
+              :total="slotProps.data.uniqueValues.size"
+              :count="slotProps.data.validValues.filter((result) => result === true).length"
+            />
+          </template>
+        </Column>
+      </DataTable>
+    </div>
   </div>
 </template>
--- src/components/DataView.vue
+++ src/components/DataView.vue
@@ -17,26 +17,28 @@ const columns = computed(() =>
 </script>
 
 <template>
-  <div>
-    <DataTable
-      :value="data"
-      size="small"
-      stripedRows
-      sortMode="multiple"
-      removableSort
-      paginator
-      :rows="5"
-      :rowsPerPageOptions="[5, 10, 50, 100]"
-    >
-      <template #empty> No data. </template>
-      <template #loading> Loading data. Please wait. </template>
-      <Column
-        v-for="(col, index) in columns"
-        :field="col.field"
-        :header="col.header"
-        :key="index"
-        sortable
-      />
-    </DataTable>
+  <div data-cy="dataView" class="card">
+    <div class="card-img-overlay" style="width: 100%">
+      <DataTable
+        :value="data"
+        size="small"
+        stripedRows
+        sortMode="multiple"
+        removableSort
+        paginator
+        :rows="5"
+        :rowsPerPageOptions="[5, 10, 50, 100]"
+      >
+        <template #empty> No data. </template>
+        <template #loading> Loading data. Please wait. </template>
+        <Column
+          v-for="(col, index) in columns"
+          :field="col.field"
+          :header="col.header"
+          :key="index"
+          sortable
+        />
+      </DataTable>
+    </div>
   </div>
 </template>
